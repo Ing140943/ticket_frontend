9b2dfc9f53a2f6b226cac08bdef8ee3269455155
Ing140943 <skladeeing@gmail.com>
2023-07-28 16:47:40 +0700

    Update UI

--- src/components/TicketForm/TicketForm.js
+++ src/components/TicketForm/TicketForm.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { createTicket, updateTicket } from "../../service/ticketService";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Select, MenuItem, InputLabel, FormControl } from "@mui/material";
+import "./TicketForm.css"
 
 export const TicketForm = ({
   currentTicket,
@@ -7,20 +12,16 @@ export const TicketForm = ({
   setFilterByStatus,
   sortBy,
   filterByStatus,
+  fetchTickets,
 }) => {
+  const statusOptions = ["Pending", "Accepted", "Resolved", "Rejected"];
+  const filterOptions = ["Pending", "Accepted", "Resolved", "Rejected", "all"];
+  const sortOptions = ["status", "latest_update", "all"];
   const [titleInput, setTitle] = useState("");
   const [descriptionInput, setDescription] = useState("");
   const [statusInput, setStatus] = useState("Pending");
   const [contactInput, setContact] = useState("");
-  //   console.log("From ticket form", currentTicket);
-
-//   console.log(titleInput, descriptionInput, contactInput, statusInput);
-//   if (Object.keys(currentTicket).length === 0) {
-//     console.log("Blank");
-//   } else {
-//     console.log(createTicket);
-//     console.log("Ticket is selected");
-//   }
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     // Set the state with the data of the current ticket when the component mounts
@@ -38,6 +39,7 @@ export const TicketForm = ({
 
   const handleClick = async () => {
     try {
+      setIsSubmitting(true);
       if (Object.keys(currentTicket).length === 0) {
         console.log("New Postttt");
         await createTicket(
@@ -60,65 +62,94 @@ export const TicketForm = ({
       setDescription("");
       setContact("");
       setStatus("Pending");
+      await fetchTickets();
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
   return (
     <div className="TicketForm">
       <div className="form">
-        <label>Sort By:</label>
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="latest_update">Latest Update</option>
-          <option value="status">Status</option>
-          <option value="all">All</option>
-        </select>
-        <label>Filter By Status:</label>
-        <select value={filterByStatus} onChange={handleFilterChange}>
-          <option value="all">All</option>
-          <option value="Pending">Pending</option>
-          <option value="Accepted">Accepted</option>
-          <option value="Resolved">Resolved</option>
-          <option value="Rejected">Rejected</option>
-        </select>
+        <Box sx={{ display: "flex", gap: "10px" }}>
+          <TextField
+            label="Title"
+            variant="outlined"
+            value={titleInput}
+            onChange={(e) => setTitle(e.target.value)}
+            size="small"
+          />
+          <TextField
+            label="Description"
+            variant="outlined"
+            value={descriptionInput}
+            onChange={(e) => setDescription(e.target.value)}
+            size="small"
+          />
+          <TextField
+            label="Contact"
+            variant="outlined"
+            value={contactInput}
+            onChange={(e) => setContact(e.target.value)}
+            size="small"
+          />
+          <FormControl size="small" variant="outlined" sx={{ minWidth: 120 }}>
+          <InputLabel htmlFor="sort-by-select">Status</InputLabel>
+            <Select
+              label="Status"
+              value={statusInput}
+              onChange={(e) => setStatus(e.target.value)}
+              variant="outlined"
+            >
+              {statusOptions.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
-        <label>Title</label>
-        <input
-          type="text"
-          name="title"
-          value={titleInput}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-
-        <label>Description</label>
-        <input
-          type="text"
-          name="description"
-          value={descriptionInput}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <label>Contact</label>
-        <input
-          type="text"
-          name="contact_info"
-          value={contactInput}
-          onChange={(e) => setContact(e.target.value)}
-        />
-
-        <label>Status</label>
-        <select
-          name="status"
-          value={statusInput}
-          onChange={(e) => setStatus(e.target.value)}
+          <FormControl size="small" variant="outlined" sx={{ minWidth: 100 }}>
+            <InputLabel htmlFor="sort-by-select">List by status:</InputLabel>
+            <Select
+              label="Filter By Status:"
+              value={filterByStatus}
+              onChange={handleFilterChange}
+              variant="outlined"
+            >
+              {filterOptions.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl size="small" variant="outlined" sx={{ minWidth: 95 }}>
+            <InputLabel htmlFor="sort-by-select">Sort By:</InputLabel>
+            <Select
+              label="Sort By:"
+              value={sortBy}
+              onChange={(e) => setSortBy(e.target.value)}
+              variant="outlined"
+            >
+              {sortOptions.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleClick()}
+          disabled={isSubmitting}
+          sx={{ marginTop: "10px" }}
         >
-          <option>Pending</option>
-          <option>Accepted</option>
-          <option>Resolved</option>
-          <option>Rejected</option>
-        </select>
-
-        <button onClick={() => handleClick()}>Submit</button>
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </Button>
       </div>
     </div>
   );
